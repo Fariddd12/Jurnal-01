d096b7c30ad9c6441d6a4ccfdf73f7b7c8ad591b
Fariddd12 <ahnaffarid45@gmail.com>
2023-09-27 09:37:37 +0700

    Kumpul Jurnal 01

---
+++ Jurnal01.java
@@ -0,0 +1,11 @@
+public class Jurnal01 {
+    public static void main (String[] args) {
+        String nama = "Ahnaf farid";
+        String nim = "607062300098";
+        String kelas = "D3IF-47-04";
+
+        System.out.println ("Nama" + nama) ;
+        System.out.println ("NIM" + nim) ;
+        System.out.println ("Kelas" + kelas) ;
+    }
+}
